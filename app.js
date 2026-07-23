7ad7e7b13604e120cb661bdf5231406b6a390034
jeily baudino <jeilybaudino.itsu@gmail.com>
2026-07-22 22:54:25 -0400

    firebase

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+// Import the functions you need from the SDKs you need
+//firebase
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA2399huLPWcqEsZNJtfR_D4Mh4RImMQN4",
+  authDomain: "bookscire.firebaseapp.com",
+  projectId: "bookscire",
+  storageBucket: "bookscire.firebasestorage.app",
+  messagingSenderId: "891645413609",
+  appId: "1:891645413609:web:40daaba270e889048e0878",
+  measurementId: "G-B1LFPCRFYX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+//firebase 
 // ==========================================
 // LÓGICA DE UI Y RENDERIZADO
 // ==========================================
